96fad20bb2e8bbbdc532f47b6162d634e0d1e2e0
aakashsaini09 <aakashsaini948585@gmail.com>
2025-09-16 11:55:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+import express from "express";
+import { createServer } from "http";
+import initSocket from './src/socket.js'
+
+const app = express();
+const port = 8000;
+
+// Create an HTTP server
+const httpServer = createServer(app);
+
+// Initialize socket.io
+initSocket(httpServer);
+
+app.get("/", (req, res) => {
+  console.log("Hello this msg for console");
+  res.send("Hello World!");
+});
+
+httpServer.listen(port, () => {
+  console.log(`🚀 Server running at http://localhost:${port}`);
+});
---
+++ src/socket.js
@@ -0,0 +1,32 @@
+// src/socket.js
+import { Server as SocketIOServer } from "socket.io";
+
+let liveUsers = 0;
+
+export default function initSocket(httpServer) {
+  const io = new SocketIOServer(httpServer, {
+    path: "/socket", // changed from /api/socket
+    cors: { origin: "*", methods: ["GET", "POST"] },
+  });
+
+  io.on("connection", (socket) => {
+    liveUsers++;
+    console.log("✅ User connected:", socket.id);
+
+    io.emit("users-count", liveUsers);
+
+    socket.on("chat-message", (text) => {
+      const message = { id: socket.id, text };
+      console.log("💬 Message:", message);
+      io.emit("chat-message", message);
+    });
+
+    socket.on("disconnect", () => {
+      liveUsers--;
+      console.log("❌ User disconnected:", socket.id);
+      io.emit("users-count", liveUsers);
+    });
+  });
+
+  console.log("🚀 Socket.io server started");
+}
